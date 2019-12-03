aefa8f7bd286c8b701f01c129fa3ee9b4f9780e0
diego <diego@mi.com.co>
2019-12-03 12:17:28 -0500

    Fix ip

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
 # rsync ./package.json ubuntu@archiva.mi.com.co:~/apps/frontpanelinterno
-rsync -r ./pages ./components ./business ubuntu@archiva.mi.com.co:~/apps/frontpanelinterno
+rsync -r ./pages ./public ./src ./package.json ubuntu@archiva.mi.com.co:~/apps/frontpanelinterno

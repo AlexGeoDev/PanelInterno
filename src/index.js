91e6402254266339ec69a07fc7f700369d53ddc3
Miguel Alejandro Pineda <miguel@mi.com.co>
2020-07-02 16:39:23 -0500

    se sube implementacion de keycloak

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Keycloak from 'keycloak-js';
 
-/* let keycloak = Keycloak('keycloak.json');
 
-function refreshToken(kc) {
-
+function refreshToken() {
   console.log("refresh keycloak token!!!");
   keycloak.updateToken(70).success((refreshed) => {
     if (refreshed) {
@@ -22,65 +20,22 @@ function refreshToken(kc) {
   });
 
   setTimeout(() => {
-    refreshToken(kc)
+    refreshToken()
   }, 60000)
+}
 
-} */
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-
-/* keycloak.init({ onLoad: 'login-required' }).success((auth) => {
+let keycloak = Keycloak('keycloak.json');
+keycloak.init({ onLoad: 'login-required' }).success((auth) => {
 
   if (!auth) {
     window.location.reload();
   } else {
     console.info("Authenticated");
   }
-
-  refreshToken(keycloak);
-
-  //start io
-  window.KEYCLOAK = keycloak;
-
-  window.URLBASE = "https://api.blazarsolutions.co/";
-
   console.log('proyecto iniciado');
-
   ReactDOM.render(<App />, document.getElementById('root'));
 
-  // If you want your app to work offline and load faster, you can change
-  // unregister() to register() below. Note this comes with some pitfalls.
-  // Learn more about service workers: https://bit.ly/CRA-PWA
-  serviceWorker.unregister();
-  /*window.start(()=>{
+  refreshToken();
+});
 
-  });*/
-
-/*
-
-  setTimeout(() => {
-    keycloak.updateToken(70).success((refreshed) => {
-      if (refreshed) {
-        console.debug('Token refreshed' + refreshed);
-      } else {
-        console.warn('Token not refreshed, valid for '
-          + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
-      }
-    }).error(() => {
-      console.error('Failed to refresh token');
-    });
-
-
-  }, 60000)
-
-}).error(() => {
-  console.error("Authenticated Failed");
-}); */
-
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();

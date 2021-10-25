e56b82eed762643a72d739ed169a09947da265d8
AlexGeoDev <jaabec@gmail.com>
2021-10-25 11:21:14 -0500

    Si infoCyclos es null entonces no aparece

--- src/view/transactionInfo/transactionInfo.js
+++ src/view/transactionInfo/transactionInfo.js
@@ -22,6 +22,7 @@ function TransactionInfo() {
 
     const fetchTrace = await fetchTransactionTrace(secuencia);
     setInfoTrace(fetchTrace.body);
+
     debugger;
   };
 
@@ -170,181 +171,221 @@ function TransactionInfo() {
               </table>
             </td>
 
-            <td>
-              <table className="ml-5">
-                <tr>
-                  <td align="center">
-                    <b>Información Cyclos</b>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    {infoCyclos && (
-                      <div>
-                        {infoCyclos.amount
-                          ? "amount: " + infoCyclos.amount
-                          : false}
-                      </div>
-                    )}
-                  </td>
-                </tr>
-
-                <tr>
-                  <td>
-                    {infoCyclos && (
-                      <div>
-                        {infoCyclos.cyclosType
-                          ? "cyclosType: " + infoCyclos.cyclosType
-                          : false}
-                      </div>
-                    )}
-                  </td>
-                </tr>
-
-                <tr>
-                  <td>
-                    {infoCyclos && (
-                      <div>
-                        {infoCyclos.date
-                          ? "date: " + formatDate(infoCyclos.date)
-                          : false}
-                      </div>
-                    )}
-                  </td>
-                </tr>
-
-                <tr>
-                  <td>
-                    {infoCyclos && (
-                      <div>
-                        {infoCyclos.description
-                          ? "description: " + infoCyclos.description
-                          : false}
-                      </div>
-                    )}
-                  </td>
-                </tr>
-
-                <tr>
-                  <td>
-                    {infoCyclos && (
-                      <div>
-                        {infoCyclos.idLVOTransaction
-                          ? "idLVOTransaction: " + infoCyclos.idLVOTransaction
-                          : false}
-                      </div>
-                    )}
-                  </td>
-                </tr>
-
-                <tr>
-                  <td>
-                    {infoCyclos && (
-                      <div>
-                        {infoCyclos.idSPTransaction
-                          ? "idSPTransaction:" + infoCyclos.idSPTransaction
-                          : false}
-                      </div>
-                    )}
-                  </td>
-                </tr>
-
-                <tr>
-                  <td>
-                    {infoCyclos && (
-                      <div>
-                        {infoCyclos.refundApproveCode
-                          ? "refundApproveCode:" + infoCyclos.refundApproveCode
-                          : false}
-                      </div>
-                    )}
-                  </td>
-                </tr>
-
-                <tr>
-                  <td>
-                    {infoCyclos && (
-                      <div>
-                        {infoCyclos.refundSequence
-                          ? "refundSequence: " + infoCyclos.refundSequence
-                          : false}
-                      </div>
-                    )}
-                  </td>
-                </tr>
-
-                <tr>
-                  <td>
-                    {infoCyclos && (
-                      <div>
-                        {infoCyclos.status
-                          ? "status: " + infoCyclos.status
-                          : false}
-                      </div>
-                    )}
-                  </td>
-                </tr>
-
-                <tr>
-                  <td>
-                    {infoCyclos && (
-                      <div>
-                        {infoCyclos.tipoMovimiento
-                          ? "tipoMovimiento: " + infoCyclos.tipoMovimiento
-                          : false}
-                      </div>
-                    )}
-                  </td>
-                </tr>
-
-                <tr>
-                  <td>
-                    {infoCyclos && (
-                      <div>
-                        {infoCyclos.txDateSP
-                          ? "txDateSP: " + infoCyclos.txDateSP
-                          : false}
-                      </div>
-                    )}
-                  </td>
-                </tr>
-
-                <tr>
-                  <td>
-                    {infoCyclos && (
-                      <div>
-                        {infoCyclos.txType
-                          ? "txType: " + infoCyclos.txType
-                          : false}
-                      </div>
-                    )}
-                  </td>
-                </tr>
-
-                <tr>
-                  <td>
-                    {infoCyclos && <div>{"Detalle de transacción: "}</div>}
-                  </td>
-                </tr>
-
-                <tr>
-                  <td align="center">
-                    {infoCyclos &&
-                      infoCyclos.listDetailDesc &&
-                      infoCyclos.listDetailDesc.map((item) => {
-                        return (
-                          <div className="d-flex align-items-center border-bottom">
-                            <div>{item.amount}</div>
-                            <div className="mx-3">
-                              {" " + formatDate(item.date)}
-                            </div>
-                            <div>{item.description}</div>
+            {infoCyclos && (
+              <td>
+                {infoCyclos ? (
+                  <table className="ml-5">
+                    <tr>
+                      <td align="center">
+                        <b> Información Cyclos</b>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        {infoCyclos && (
+                          <div>
+                            {infoCyclos.amount
+                              ? "amount: " + infoCyclos.amount
+                              : false}
                           </div>
-                        );
-                      })}
-                  </td>
-                </tr>
-              </table>
+                        )}
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        {infoCyclos && (
+                          <div>
+                            {infoCyclos.cyclosType
+                              ? "cyclosType: " + infoCyclos.cyclosType
+                              : false}
+                          </div>
+                        )}
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        {infoCyclos && (
+                          <div>
+                            {infoCyclos.date
+                              ? "date: " + formatDate(infoCyclos.date)
+                              : false}
+                          </div>
+                        )}
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        {infoCyclos && (
+                          <div>
+                            {infoCyclos.description
+                              ? "description: " + infoCyclos.description
+                              : false}
+                          </div>
+                        )}
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        {infoCyclos && (
+                          <div>
+                            {infoCyclos.idLVOTransaction
+                              ? "idLVOTransaction: " +
+                                infoCyclos.idLVOTransaction
+                              : false}
+                          </div>
+                        )}
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        {infoCyclos && (
+                          <div>
+                            {infoCyclos.idSPTransaction
+                              ? "idSPTransaction:" + infoCyclos.idSPTransaction
+                              : false}
+                          </div>
+                        )}
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        {infoCyclos && (
+                          <div>
+                            {infoCyclos.refundApproveCode
+                              ? "refundApproveCode:" +
+                                infoCyclos.refundApproveCode
+                              : false}
+                          </div>
+                        )}
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        {infoCyclos && (
+                          <div>
+                            {infoCyclos.refundSequence
+                              ? "refundSequence: " + infoCyclos.refundSequence
+                              : false}
+                          </div>
+                        )}
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        {infoCyclos && (
+                          <div>
+                            {infoCyclos.status
+                              ? "status: " + infoCyclos.status
+                              : false}
+                          </div>
+                        )}
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        {infoCyclos && (
+                          <div>
+                            {infoCyclos.tipoMovimiento
+                              ? "tipoMovimiento: " + infoCyclos.tipoMovimiento
+                              : false}
+                          </div>
+                        )}
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        {infoCyclos && (
+                          <div>
+                            {infoCyclos.txDateSP
+                              ? "txDateSP: " + infoCyclos.txDateSP
+                              : false}
+                          </div>
+                        )}
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        {infoCyclos && (
+                          <div>
+                            {infoCyclos.txType
+                              ? "txType: " + infoCyclos.txType
+                              : false}
+                          </div>
+                        )}
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td>
+                        {infoCyclos && <div>{"Detalle de transacción: "}</div>}
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td align="center">
+                        {infoCyclos &&
+                          infoCyclos.listDetailDesc &&
+                          infoCyclos.listDetailDesc.map((item) => {
+                            return (
+                              <div className="d-flex align-items-center border-bottom">
+                                <div>{item.amount}</div>
+                                <div className="mx-3">
+                                  {" " + formatDate(item.date)}
+                                </div>
+                                <div>{item.description}</div>
+                              </div>
+                            );
+                          })}
+                      </td>
+                    </tr>
+                  </table>
+                ) : (
+                  false
+                )}
+              </td>
+            )}
+          </tr>
+        </table>
+
+        <table className="mt-5">
+          <tr>
+            <td align="center">
+              <b>Histórico de datos</b>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              {infoTrace &&
+                infoTrace.map((item) => {
+                  return (
+                    <table>
+                      <tr>
+                        <td>{"Estado: " + item.detail}</td>
+                      </tr>
+
+                      <tr>
+                        <td>
+                          {"Fecha de registro: " +
+                            formatDate(item.registerDate)}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>{"Secuencia: " + item.sequence}</td>
+                      </tr>
+                    </table>
+                  );
+                })}
             </td>
           </tr>
         </table>
